69de434d6936948883570bcd260d6667c297164c
weibin268 <binweizhuang@hotmail.com>
2018-01-30 13:50:37 +0800

    aa

---
+++ Zhuang.Commons.Test/Utils/CmdUtilTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Zhuang.Commons.Utils;
+
+
+namespace Zhuang.Commons.Test.Utils
+{
+    [TestClass]
+    public class CmdUtilTest
+    {
+        [TestMethod]
+        public void Exec()
+        {
+            Console.WriteLine( CmdUtil.Exec("dir"));
+        }
+    }
+}
---
+++ Zhuang.Commons/Utils/CmdUtil.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Zhuang.Commons.Utils
+{
+    public class CmdUtil
+    {
+        public static string Exec(string cmd)
+        {
+            StringBuilder sbResult=new StringBuilder();
+
+            Process proc = new Process
+            {
+                StartInfo =
+                {
+                    FileName = "cmd.exe",
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true
+                }
+            };
+
+            proc.Start();
+
+            proc.StandardInput.AutoFlush = true;
+            proc.StandardInput.WriteLine(cmd + "&exit");
+
+            sbResult.Append(proc.StandardOutput.ReadToEnd());
+
+            sbResult.Append(proc.StandardError.ReadToEnd());
+
+            while (!proc.HasExited)
+            {
+                proc.WaitForExit(1000);
+            }
+            
+            return sbResult.ToString();
+        }
+    }
+}
